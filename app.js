857fd14f472833984ca780a16bec29827c98cb45
Park-JunHyeong <115024272+iamjunhyeong@users.noreply.github.com>
2023-02-25 16:38:24 +0000

    localStorage

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const loginform = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 function btnclick(event) {
     event.preventDefault();
+    loginform.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    console.log(username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
-loginform.addEventListener("submit", btnclick)
+function paintGreetings(username) {
+    greeting.innerHTML = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+    loginform.classList.remove(HIDDEN_CLASSNAME);
+    loginform.addEventListener("submit", btnclick)
+} else {
+    paintGreetings(savedUserName);
+}
+
+
+
+
