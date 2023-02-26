29053a897b54aaa7dfe621d16060195a477eed4f
Park-JunHyeong <115024272+iamjunhyeong@users.noreply.github.com>
2023-02-26 08:24:44 +0000

    greeting

--- js/greeting.js
+++ js/greeting.js
@@ -1,8 +1,10 @@
 const loginform = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
+
 function btnclick(event) {
     event.preventDefault();
     loginform.classList.add(HIDDEN_CLASSNAME);
@@ -11,7 +13,7 @@ function btnclick(event) {
     paintGreetings(username);
 }
 function paintGreetings(username) {
-    greeting.innerHTML = `Hello ${username}`
+    greeting.innerText = `Hello ${username}`
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
