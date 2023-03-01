3a029680c6bb4e90cd17809016b8def63072db98
Park-JunHyeong <115024272+iamjunhyeong@users.noreply.github.com>
2023-03-01 14:39:50 +0000

    weather

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+function onGeoOK(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+    // url 정보를 fetch를 통해서 불러옴
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK,onGeoError);
