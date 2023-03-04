3db0c04a8be224ff1c30832e0f0bb181826712e9
iamjunhyeong <dddodo447@gmail.com>
2023-03-04 22:30:04 +0900

    2023.03.04

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,9 @@
 function onGeoOK(position) {
+    const API_KEY = "433890a98e8206808082b38eddd9caa3";
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
 
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
-    console.log(url);
     // url 정보를 fetch를 통해서 불러옴
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child");
